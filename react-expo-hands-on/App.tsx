f964eff704d5470d7976dd6656e30e2775b1c2d6
Sagar Teria <sagarteria@gmail.com>
2025-07-27 18:46:46 +0000

    Add RegistrationScreen and update navigation in App

--- react-expo-hands-on/App.tsx
+++ react-expo-hands-on/App.tsx
@@ -5,6 +5,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './screens/HomeScreen';
 import AboutScreen from './screens/AboutScreen';
 import ContactScreen from './screens/ContactScreen';
+import RegistrationScreen from './screens/RegistrationScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -15,6 +16,7 @@ export default function App() {
        <Stack.Screen name="Home" component={HomeScreen} />
        <Stack.Screen name="About" component={AboutScreen} />
        <Stack.Screen name="Contact" component={ContactScreen} />
+       <Stack.Screen name="Register" component={RegistrationScreen} />
      </Stack.Navigator>
    </NavigationContainer>
  );
